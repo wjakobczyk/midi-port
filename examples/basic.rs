cbfdba165afdda9fd4f81e6d58733421e1a98072
Wojciech Jakobczyk <jakobczyk.woj@gmail.com>
2019-12-01 14:32:26 +0100

    Initial implementation, basic example

--- examples/basic.rs
+++ examples/basic.rs
@@ -7,20 +7,18 @@ use cortex_m::peripheral::Peripherals;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
-
-use hal::delay::Delay;
 use hal::gpio::*;
 use hal::rcc::RccExt;
-use hal::stm32;
-use hal::serial::*;
 use hal::serial::config::*;
+use hal::serial::*;
+use hal::stm32;
 use stm32f4xx_hal as hal;
 
-use embedded_hal::serial::Read;
+use midi_port::*;
 
 #[entry]
 fn main() -> ! {
-    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+    if let (Some(p), Some(_cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
         let rcc = p.RCC.constrain();
 
         let clocks = rcc
@@ -28,13 +26,11 @@ fn main() -> ! {
             .sysclk(stm32f4xx_hal::time::MegaHertz(168))
             .freeze();
 
-        let mut delay = Delay::new(cp.SYST, clocks);
-
         let gpioc = p.GPIOC.split();
 
         let rx_pin = gpioc.pc11.into_alternate_af8();
 
-        let mut uart = Serial::uart4(
+        let uart = Serial::uart4(
             p.UART4,
             (NoTx, rx_pin),
             Config {
@@ -44,15 +40,25 @@ fn main() -> ! {
                 stopbits: StopBits::STOP1,
             },
             clocks,
-        ).unwrap();
+        )
+        .unwrap();
+
+        let mut midi_in = MidiInPort::new(uart);
 
         loop {
-            match (uart.read()) {
-                Ok(byte) => hprintln!("{:x}", byte).unwrap(),
-                Err(e) => (),
-            };
-            
+            midi_in.poll_uart();
+
+            if let Some(message) = midi_in.get_message() {
+                hprintln!("{:?}", message).unwrap();
+            }
         }
+
+        // loop {
+        //     match (uart.read()) {
+        //         Ok(byte) => hprintln!("{:x}", byte).unwrap(),
+        //         Err(e) => (),
+        //     };
+        // }
     }
 
     panic!();
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,97 @@
 //!
 
 #![no_std]
+
+use embedded_hal::serial::Read;
+
+type NoteNumber = u8;
+type ControllerNumber = u8;
+
+const MAX_MSG_SIZE: usize = 3;
+
+#[derive(Debug)]
+pub enum MidiMessage {
+    NoteOn {
+        note: NoteNumber,
+        velocity: u8,
+    },
+    NoteOff {
+        note: NoteNumber,
+        velocity: u8,
+    },
+    ControlChange {
+        controller: ControllerNumber,
+        value: u8,
+    },
+    Unknown,
+}
+
+pub struct MidiInPort<UART: Read<u8>> {
+    uart: UART,
+    buffer: [u8; MAX_MSG_SIZE],
+    in_buffer: usize,
+    message: Option<MidiMessage>,
+}
+
+impl<UART: Read<u8>> MidiInPort<UART> {
+    pub fn new(uart: UART) -> Self {
+        MidiInPort {
+            uart,
+            buffer: [0; MAX_MSG_SIZE],
+            in_buffer: 0,
+            message: None,
+        }
+    }
+
+    pub fn get_message(&mut self) -> Option<MidiMessage> {
+        self.message.take()
+    }
+
+    fn put_byte(&mut self, byte: u8) {
+        self.buffer[self.in_buffer] = byte;
+        self.in_buffer += 1;
+
+        if self.expected_message_size() == self.in_buffer {
+            self.create_message();
+            self.in_buffer = 0;
+        }
+    }
+
+    fn expected_message_size(&self) -> usize {
+        let hi = self.buffer[0] >> 4;
+        let lo = self.buffer[0] & 0xf;
+
+        if hi == 0b1000 || hi == 0b1000 {
+            3
+        } else {
+            1
+        }
+    }
+
+    fn create_message(&mut self) {
+        let hi = self.buffer[0] >> 4;
+        let lo = self.buffer[0] & 0xf;
+
+        self.message = if hi == 0b1001 {
+            Some(MidiMessage::NoteOn {
+                note: self.buffer[1],
+                velocity: self.buffer[2],
+            })
+        } else if hi == 0b1000 {
+            Some(MidiMessage::NoteOff {
+                note: self.buffer[1],
+                velocity: self.buffer[2],
+            })
+        } else {
+            Some(MidiMessage::Unknown)
+        }
+    }
+
+    pub fn poll_uart(&mut self) {
+        let byte = self.uart.read();
+
+        if let Ok(byte) = byte {
+            self.put_byte(byte);
+        }
+    }
+}
