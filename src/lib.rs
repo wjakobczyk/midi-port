7c3ca2e22ea4788d1cc058bf60e8976117f1dafa
Wojciech Jakobczyk <jakobczyk.woj@gmail.com>
2019-12-07 19:53:58 +0100

    More voice messages

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,72 @@
 
 use embedded_hal::serial::Read;
 
-type NoteNumber = u8;
-type ControllerNumber = u8;
+use num_derive::FromPrimitive;    
+use num_traits::FromPrimitive;
+
+pub type ChannelNumber = u8;
+pub type NoteNumber = u8;
+pub type ControllerNumber = u8;
+pub type ProgramNumber = u8;
 
 const MAX_MSG_SIZE: usize = 3;
 
 #[derive(Debug)]
 pub enum MidiMessage {
     NoteOn {
+        channel: ChannelNumber,
         note: NoteNumber,
         velocity: u8,
     },
     NoteOff {
+        channel: ChannelNumber,
         note: NoteNumber,
         velocity: u8,
     },
+    Aftertouch {
+        channel: ChannelNumber,
+        note: Option<NoteNumber>,   //Null for channel aftertouch
+        value: u8,
+    },
     ControlChange {
+        channel: ChannelNumber,
         controller: ControllerNumber,
         value: u8,
     },
+    ProgramChange {
+        channel: ChannelNumber,
+        program: ProgramNumber,
+    },
+    PitchBendChange {
+        channel: ChannelNumber,
+        value: u16,     //14 bits used
+    },
     Unknown,
 }
+
+#[derive(FromPrimitive)]
+enum Status {
+    NoteOff = 0x80,
+    NoteOn = 0x90,
+    PolyphonicAftertouch = 0xA0,
+    ControlChange = 0xB0,
+    ProgramChange = 0xC0,
+    ChannelAftertouch = 0xD0,
+    PitchBend = 0xE0,
+    SysExStart = 0xF0,
+    TimeCodeQtrFrame = 0xF1,
+    SongPositionPointer = 0xF2,
+    SongSelect = 0xF3,
+    TuneRequest = 0xF6,
+    SysExEnd = 0xF7,
+    TimingClock = 0xF8,
+    Start = 0xFA,
+    Continue = 0xFB,
+    Stop = 0xFC,
+    ActiveSensing = 0xFE,
+    SystemReset = 0xFF,
+}
+
 
 pub struct MidiInPort<UART: Read<u8>> {
     uart: UART,
@@ -59,32 +104,60 @@ impl<UART: Read<u8>> MidiInPort<UART> {
     }
 
     fn expected_message_size(&self) -> usize {
-        let hi = self.buffer[0] >> 4;
-        let lo = self.buffer[0] & 0xf;
+        let hi = self.buffer[0] & 0xf0;
 
-        if hi == 0b1000 || hi == 0b1000 {
-            3
-        } else {
-            1
+        match FromPrimitive::from_u8(hi) {
+            Some(Status::NoteOn) => 3,
+            Some(Status::NoteOff) => 3,
+            Some(Status::PolyphonicAftertouch) => 3,
+            Some(Status::ControlChange) => 3,
+            Some(Status::ChannelAftertouch) => 2,
+            Some(Status::PitchBend) => 3,
+            _ => 1,
         }
     }
 
     fn create_message(&mut self) {
-        let hi = self.buffer[0] >> 4;
+        let hi = self.buffer[0] & 0xf0;
         let lo = self.buffer[0] & 0xf;
 
-        self.message = if hi == 0b1001 {
-            Some(MidiMessage::NoteOn {
-                note: self.buffer[1],
-                velocity: self.buffer[2],
-            })
-        } else if hi == 0b1000 {
-            Some(MidiMessage::NoteOff {
-                note: self.buffer[1],
-                velocity: self.buffer[2],
-            })
-        } else {
-            Some(MidiMessage::Unknown)
+        self.message = match FromPrimitive::from_u8(hi) {
+            Some(Status::NoteOn) => 
+                Some(MidiMessage::NoteOn {
+                    channel: lo,
+                    note: self.buffer[1],
+                    velocity: self.buffer[2],
+                }),
+            Some(Status::NoteOff) => 
+                Some(MidiMessage::NoteOff {
+                    channel: lo,
+                    note: self.buffer[1],
+                    velocity: self.buffer[2],
+                }),
+            Some(Status::PolyphonicAftertouch) => 
+                Some(MidiMessage::Aftertouch {
+                    channel: lo,
+                    note: Some(self.buffer[1]),
+                    value: self.buffer[2],
+                }),
+            Some(Status::ControlChange) => 
+                Some(MidiMessage::ControlChange {
+                    channel: lo,
+                    controller: self.buffer[1],
+                    value: self.buffer[2],
+                }),
+            Some(Status::ChannelAftertouch) => 
+                Some(MidiMessage::Aftertouch {
+                    channel: lo,
+                    note: None,
+                    value: self.buffer[1],
+                }),
+            Some(Status::PitchBend) => 
+                Some(MidiMessage::PitchBendChange {
+                    channel: lo,
+                    value: self.buffer[1] as u16 + (self.buffer[2] as u16) << 7,
+                }),
+        _ => Some(MidiMessage::Unknown),
         }
     }
 
